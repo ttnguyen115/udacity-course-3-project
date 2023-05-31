548e1a563d152cfb328644d1ad1bfe26ad90e04d
ttnguyen115 <thanhtrung1012000@gmail.com>
2023-05-31 10:29:31 +0700

    configure container orchestration with kubernetes

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://ac1770bce8ef946878e6323db5b9b1b5-356488251.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
